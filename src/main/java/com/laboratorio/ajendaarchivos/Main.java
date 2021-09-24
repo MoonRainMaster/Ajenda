9b9fb134f536ab565f341bbe77923da218007698
MoonRain <brianbatz03@gmail.com>
2021-09-24 15:23:29 -0600

    Archivo main creado

--- src/main/java/com/laboratorio/ajendaarchivos/Main.java
+++ src/main/java/com/laboratorio/ajendaarchivos/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.laboratorio.ajendaarchivos;
 
 /**
@@ -9,5 +5,7 @@ package com.laboratorio.ajendaarchivos;
  * @author moonrain
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
